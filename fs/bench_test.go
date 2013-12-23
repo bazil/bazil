9c54faeb2ce1ae6be6f5032ca3d032e0864ec0db
Tommi Virtanen <tv@eagain.net>
2013-12-22 19:30:30 -0800

    fs bench: Add simple file read benchmark

--- fs/bench_test.go
+++ fs/bench_test.go
@@ -2,6 +2,7 @@ package fs_test
 
 import (
 	"io"
+	"io/ioutil"
 	"os"
 	"path"
 	"testing"
@@ -54,4 +55,68 @@ func BenchmarkWrite10MB(b *testing.B) {
 
 func BenchmarkWrite100MB(b *testing.B) {
 	benchmarkWrite(b, 100*1024*1024)
+}
+
+func benchmarkRead(b *testing.B, size int64) {
+	tmp := tempdir.New(b)
+	defer tmp.Cleanup()
+	app := bazfstestutil.NewApp(b, tmp.Subdir("data"))
+	defer app.Close()
+
+	mnt := bazfstestutil.Mounted(b, app)
+	defer mnt.Close()
+
+	p := path.Join(mnt.Dir, "testcontent")
+
+	{
+		counter := &bazfstestutil.CountReader{}
+		f, err := os.Create(p)
+		if err != nil {
+			b.Fatalf("create: %v", err)
+		}
+		defer f.Close()
+		_, err = io.CopyN(f, counter, size)
+		if err != nil {
+			b.Fatalf("read: %v", err)
+		}
+		err = f.Close()
+		if err != nil {
+			b.Fatalf("close: %v", err)
+		}
+	}
+
+	b.ResetTimer()
+	b.SetBytes(size)
+
+	for i := 0; i < b.N; i++ {
+		f, err := os.Open(p)
+		if err != nil {
+			b.Fatalf("close: %v", err)
+		}
+
+		n, err := io.Copy(ioutil.Discard, f)
+		if err != nil {
+			b.Fatalf("read: %v", err)
+		}
+		if n != size {
+			b.Errorf("unexpected size: %d != %d", n, size)
+		}
+
+		err = f.Close()
+		if err != nil {
+			b.Fatalf("close: %v", err)
+		}
+	}
+}
+
+func BenchmarkRead100(b *testing.B) {
+	benchmarkRead(b, 100)
+}
+
+func BenchmarkRead10MB(b *testing.B) {
+	benchmarkRead(b, 10*1024*1024)
+}
+
+func BenchmarkRead100MB(b *testing.B) {
+	benchmarkRead(b, 100*1024*1024)
 }
