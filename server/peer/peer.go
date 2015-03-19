2185f764b9483b29df86ed77a3f16e913f544790
Tommi Virtanen <tv@eagain.net>
2015-03-19 14:57:21 -0700

    server/peer: Use grpcedtls to authenticate peers

--- server/peer/peer.go
+++ server/peer/peer.go
@@ -1,17 +1,42 @@
 package peer
 
 import (
+	"bazil.org/bazil/peer"
 	"bazil.org/bazil/peer/wire"
 	"bazil.org/bazil/server"
+	"bazil.org/bazil/util/grpcedtls"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
+
+func (p *peers) auth(ctx context.Context) (*peer.Peer, error) {
+	pub, ok := grpcedtls.FromContext(ctx)
+	if !ok {
+		return nil, grpc.Errorf(codes.Unauthenticated, "unauthenticated")
+	}
+	peer, err := p.app.GetPeer(pub)
+	if err == server.ErrPeerNotFound {
+		return nil, grpc.Errorf(codes.PermissionDenied, "permission denied")
+	}
+	if err != nil {
+		return nil, err
+	}
+	return peer, nil
+}
 
 type peers struct {
 	app *server.App
 }
 
 func New(app *server.App) *grpc.Server {
-	srv := grpc.NewServer()
+	auth := &grpcedtls.Authenticator{
+		Config: app.GetTLSConfig,
+		// TODO Lookup:
+	}
+	srv := grpc.NewServer(
+		grpc.WithServerTransportAuthenticator(auth),
+	)
 	rpc := &peers{app: app}
 	wire.RegisterPeerServer(srv, rpc)
 	return srv
--- server/peer/ping.go
+++ server/peer/ping.go
@@ -6,5 +6,9 @@ import (
 )
 
 func (p *peers) Ping(ctx context.Context, req *wire.PingRequest) (*wire.PingResponse, error) {
+	_, err := p.auth(ctx)
+	if err != nil {
+		return nil, err
+	}
 	return &wire.PingResponse{}, nil
 }
