656b72348a14e1fa8952537c0f595b9226c1e97e
Tommi Virtanen <tv@eagain.net>
2015-03-21 20:31:48 -0700

    cli/peer/add, main: CLI to add peers

---
+++ cli/peer/add/add.go
@@ -0,0 +1,40 @@
+package add
+
+import (
+	clibazil "bazil.org/bazil/cli"
+	"bazil.org/bazil/cliutil/subcommands"
+	"bazil.org/bazil/control/wire"
+	"bazil.org/bazil/peer"
+	"golang.org/x/net/context"
+)
+
+type addCommand struct {
+	subcommands.Description
+	Arguments struct {
+		PubKey peer.PublicKey
+	}
+}
+
+func (cmd *addCommand) Run() error {
+	req := &wire.PeerAddRequest{
+		Pub: cmd.Arguments.PubKey[:],
+	}
+	ctx := context.Background()
+	client, err := clibazil.Bazil.Control()
+	if err != nil {
+		return err
+	}
+	if _, err := client.PeerAdd(ctx, req); err != nil {
+		// TODO unwrap error
+		return err
+	}
+	return nil
+}
+
+var add = addCommand{
+	Description: "add a peer",
+}
+
+func init() {
+	subcommands.Register(&add)
+}
