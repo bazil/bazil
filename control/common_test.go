bd7c63004bad72d433cd5b104220afc5b5a28271
Tommi Virtanen <tv@eagain.net>
2015-03-21 17:56:26 -0700

    control test: Refactor

---
+++ control/common_test.go
@@ -0,0 +1,49 @@
+package control
+
+import (
+	"fmt"
+	"testing"
+
+	"bazil.org/bazil/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+func controlListenAndServe(t testing.TB, app *server.App) (stop func()) {
+	c, err := New(app)
+	if err != nil {
+		t.Fatalf("control socket cannot listen: %v", err)
+	}
+	serveErr := make(chan error, 1)
+	go func() {
+		serveErr <- c.Serve()
+	}()
+
+	quit := make(chan struct{})
+	go func() {
+		select {
+		case <-quit:
+			c.Close()
+			_ = <-serveErr
+
+		case err := <-serveErr:
+			if err != nil {
+				t.Errorf("control socket serve: %v", err)
+			}
+		}
+	}()
+
+	return func() {
+		close(quit)
+	}
+}
+
+func checkRPCError(t testing.TB, err error, code codes.Code, message string) {
+	if g, e := grpc.Code(err), code; g != e {
+		t.Errorf("wrong grpc error code: %v != %v", g, e)
+	}
+	// TODO https://github.com/grpc/grpc-go/issues/110
+	if g, e := err.Error(), fmt.Sprintf("rpc error: code = %d desc = %q", grpc.Code(err), message); g != e {
+		t.Errorf("wrong error message: %v != %v", g, e)
+	}
+}
