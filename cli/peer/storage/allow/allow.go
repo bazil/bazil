6a372e25753ed049e21afcbd02389ac8636d922d
Tommi Virtanen <tv@eagain.net>
2015-03-26 20:57:40 -0700

    cli/peer/storage/allow, main: CLI to allow peers to store objects

---
+++ cli/peer/storage/allow/allow.go
@@ -0,0 +1,46 @@
+package allow
+
+import (
+	clibazil "bazil.org/bazil/cli"
+	"bazil.org/bazil/cliutil/subcommands"
+	"bazil.org/bazil/control/wire"
+	"bazil.org/bazil/peer"
+	wireserver "bazil.org/bazil/server/wire"
+	"golang.org/x/net/context"
+)
+
+type allowCommand struct {
+	subcommands.Description
+	Arguments struct {
+		PubKey  peer.PublicKey
+		Storage string
+	}
+}
+
+func (cmd *allowCommand) Run() error {
+	req := &wire.PeerStorageAllowRequest{
+		Pub: cmd.Arguments.PubKey[:],
+		Backends: &wireserver.PeerStorage{
+			Backends: map[string]*wireserver.PeerStorageConfig{},
+		},
+	}
+	req.Backends.Backends[cmd.Arguments.Storage] = &wireserver.PeerStorageConfig{}
+	ctx := context.Background()
+	client, err := clibazil.Bazil.Control()
+	if err != nil {
+		return err
+	}
+	if _, err := client.PeerStorageAllow(ctx, req); err != nil {
+		// TODO unwrap error
+		return err
+	}
+	return nil
+}
+
+var allow = allowCommand{
+	Description: "allow a peer",
+}
+
+func init() {
+	subcommands.Register(&allow)
+}
