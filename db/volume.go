e84cb109d4536e63fcb60c89ad6ddcee946058a3
Tommi Virtanen <tv@eagain.net>
2015-05-29 10:47:50 -0700

    db: Refactor to split large volume file

--- db/volume.go
+++ db/volume.go
@@ -4,18 +4,15 @@ import (
 	"crypto/rand"
 	"errors"
 
-	"bazil.org/bazil/db/wire"
 	"bazil.org/bazil/tokens"
 	"github.com/boltdb/bolt"
-	"github.com/gogo/protobuf/proto"
 )
 
 var (
-	ErrVolNameInvalid     = errors.New("invalid volume name")
-	ErrVolNameNotFound    = errors.New("volume name not found")
-	ErrVolNameExist       = errors.New("volume name exists already")
-	ErrVolumeIDNotFound   = errors.New("volume ID not found")
-	ErrVolumeStorageExist = errors.New("volume storage name exists already")
+	ErrVolNameInvalid   = errors.New("invalid volume name")
+	ErrVolNameNotFound  = errors.New("volume name not found")
+	ErrVolNameExist     = errors.New("volume name exists already")
+	ErrVolumeIDNotFound = errors.New("volume ID not found")
 )
 
 var (
@@ -168,87 +165,4 @@ func (v *Volume) InodeBucket() *bolt.Bucket {
 // SnapBucket returns a bolt bucket for storing snapshots in.
 func (v *Volume) SnapBucket() *bolt.Bucket {
 	return v.b.Bucket(volumeStateSnap)
-}
-
-type VolumeStorage struct {
-	b *bolt.Bucket
-}
-
-// Add a storage backend to be used by the volume.
-//
-// Active Volume instances are not notified.
-//
-// If volume has storage by that name already, returns
-// ErrVolumeStorageExist.
-func (vs *VolumeStorage) Add(name string, backend string, sharingKey *SharingKey) error {
-	n := []byte(name)
-	if v := vs.b.Get(n); v != nil {
-		return ErrVolumeStorageExist
-	}
-	msg := &wire.VolumeStorage{
-		Backend:        backend,
-		SharingKeyName: sharingKey.Name(),
-	}
-	buf, err := proto.Marshal(msg)
-	if err != nil {
-		return err
-	}
-	return vs.b.Put(n, buf)
-}
-
-func (vs *VolumeStorage) Cursor() *VolumeStorageCursor {
-	return &VolumeStorageCursor{vs.b.Cursor()}
-}
-
-type VolumeStorageCursor struct {
-	c *bolt.Cursor
-}
-
-func (c *VolumeStorageCursor) item(k, v []byte) *VolumeStorageItem {
-	if k == nil {
-		return nil
-	}
-	return &VolumeStorageItem{name: k, data: v}
-}
-
-func (c *VolumeStorageCursor) First() *VolumeStorageItem {
-	return c.item(c.c.First())
-}
-
-func (c *VolumeStorageCursor) Next() *VolumeStorageItem {
-	return c.item(c.c.Next())
-}
-
-type VolumeStorageItem struct {
-	name []byte
-	data []byte
-	conf wire.VolumeStorage
-}
-
-func (item *VolumeStorageItem) unmarshal() error {
-	return proto.Unmarshal(item.data, &item.conf)
-}
-
-// Backend returns the storage backend for this item.
-//
-// Returned value is valid after the transaction.
-func (item *VolumeStorageItem) Backend() (string, error) {
-	if item.conf.Backend == "" {
-		if err := item.unmarshal(); err != nil {
-			return "", err
-		}
-	}
-	return item.conf.Backend, nil
-}
-
-// SharingKeyName returns the sharing key name for this item.
-//
-// Returned value is valid after the transaction.
-func (item *VolumeStorageItem) SharingKeyName() (string, error) {
-	if item.conf.Backend == "" {
-		if err := item.unmarshal(); err != nil {
-			return "", err
-		}
-	}
-	return item.conf.SharingKeyName, nil
 }
---
+++ db/volumeStorage.go
@@ -0,0 +1,96 @@
+package db
+
+import (
+	"errors"
+
+	"bazil.org/bazil/db/wire"
+	"github.com/boltdb/bolt"
+	"github.com/gogo/protobuf/proto"
+)
+
+var (
+	ErrVolumeStorageExist = errors.New("volume storage name exists already")
+)
+
+type VolumeStorage struct {
+	b *bolt.Bucket
+}
+
+// Add a storage backend to be used by the volume.
+//
+// Active Volume instances are not notified.
+//
+// If volume has storage by that name already, returns
+// ErrVolumeStorageExist.
+func (vs *VolumeStorage) Add(name string, backend string, sharingKey *SharingKey) error {
+	n := []byte(name)
+	if v := vs.b.Get(n); v != nil {
+		return ErrVolumeStorageExist
+	}
+	msg := &wire.VolumeStorage{
+		Backend:        backend,
+		SharingKeyName: sharingKey.Name(),
+	}
+	buf, err := proto.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	return vs.b.Put(n, buf)
+}
+
+func (vs *VolumeStorage) Cursor() *VolumeStorageCursor {
+	return &VolumeStorageCursor{vs.b.Cursor()}
+}
+
+type VolumeStorageCursor struct {
+	c *bolt.Cursor
+}
+
+func (c *VolumeStorageCursor) item(k, v []byte) *VolumeStorageItem {
+	if k == nil {
+		return nil
+	}
+	return &VolumeStorageItem{name: k, data: v}
+}
+
+func (c *VolumeStorageCursor) First() *VolumeStorageItem {
+	return c.item(c.c.First())
+}
+
+func (c *VolumeStorageCursor) Next() *VolumeStorageItem {
+	return c.item(c.c.Next())
+}
+
+type VolumeStorageItem struct {
+	name []byte
+	data []byte
+	conf wire.VolumeStorage
+}
+
+func (item *VolumeStorageItem) unmarshal() error {
+	return proto.Unmarshal(item.data, &item.conf)
+}
+
+// Backend returns the storage backend for this item.
+//
+// Returned value is valid after the transaction.
+func (item *VolumeStorageItem) Backend() (string, error) {
+	if item.conf.Backend == "" {
+		if err := item.unmarshal(); err != nil {
+			return "", err
+		}
+	}
+	return item.conf.Backend, nil
+}
+
+// SharingKeyName returns the sharing key name for this item.
+//
+// Returned value is valid after the transaction.
+func (item *VolumeStorageItem) SharingKeyName() (string, error) {
+	if item.conf.Backend == "" {
+		if err := item.unmarshal(); err != nil {
+			return "", err
+		}
+	}
+	return item.conf.SharingKeyName, nil
+}
