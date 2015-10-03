69c845523436d6d8fa3d26ed849f39e29ed48fe6
Tommi Virtanen <tv@eagain.net>
2015-10-02 19:00:03 -0700

    fs test: Add test for syncing a rename with complex clock
    This used to fail before the work completed in previous commits.

--- fs/sync_test.go
+++ fs/sync_test.go
@@ -1,6 +1,7 @@
 package fs_test
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
@@ -764,6 +765,188 @@ func TestSyncRename(t *testing.T) {
 			t.Fatalf("cannot read file: %v", err)
 		}
 		if g, e := string(buf), input; g != e {
+			t.Fatalf("wrong content: %q != %q", g, e)
+		}
+	}
+}
+
+func TestSyncRenameWithResolvedConflict(t *testing.T) {
+	tmp := tempdir.New(t)
+	defer tmp.Cleanup()
+	app1 := bazfstestutil.NewAppWithName(t, tmp.Subdir("app1"), "1")
+	defer app1.Close()
+	app2 := bazfstestutil.NewAppWithName(t, tmp.Subdir("app2"), "2")
+	defer app2.Close()
+
+	pub1 := (*peer.PublicKey)(app1.Keys.Sign.Pub)
+	pub2 := (*peer.PublicKey)(app2.Keys.Sign.Pub)
+
+	const (
+		volumeName1 = "testvol1"
+		volumeName2 = "testvol2"
+	)
+	createAndConnectVolume(t, app1, volumeName1, app2, volumeName2)
+	connectVolume(t, app2, volumeName2, app1, volumeName1)
+
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	web1 := httptest.ServeHTTP(t, &wg, app1)
+	defer web1.Close()
+	setLocation(t, app2, app1.Keys.Sign.Pub, web1.Addr())
+
+	web2 := httptest.ServeHTTP(t, &wg, app2)
+	defer web2.Close()
+	setLocation(t, app1, app2.Keys.Sign.Pub, web2.Addr())
+
+	mnt1 := bazfstestutil.Mounted(t, app1, volumeName1)
+	defer mnt1.Close()
+	mnt2 := bazfstestutil.Mounted(t, app2, volumeName2)
+	defer mnt2.Close()
+
+	const (
+		filename1 = "greeting"
+		filename2 = "cheers"
+		input1    = "hello, world"
+		input2    = "goodbye"
+		input3    = "farewell"
+	)
+
+	if err := ioutil.WriteFile(path.Join(mnt1.Dir, filename1), []byte(input1), 0644); err != nil {
+		t.Fatalf("cannot create file: %v", err)
+	}
+
+	// trigger sync
+	ctrl2 := controltest.ListenAndServe(t, &wg, app2)
+	defer ctrl2.Close()
+	rpcConn2, err := grpcunix.Dial(filepath.Join(app2.DataDir, "control"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rpcConn2.Close()
+	rpcClient2 := wire.NewControlClient(rpcConn2)
+	{
+		ctx := context.Background()
+		req := &wire.VolumeSyncRequest{
+			VolumeName: volumeName2,
+			Pub:        pub1[:],
+		}
+		if _, err := rpcClient2.VolumeSync(ctx, req); err != nil {
+			t.Fatalf("error while syncing: %v", err)
+		}
+	}
+
+	{
+		buf, err := ioutil.ReadFile(path.Join(mnt2.Dir, filename1))
+		if err != nil {
+			t.Fatalf("cannot read file: %v", err)
+		}
+		if g, e := string(buf), input1; g != e {
+			t.Fatalf("wrong content: %q != %q", g, e)
+		}
+	}
+
+	{
+		check := map[string]bazfstestutil.FileInfoCheck{
+			filename1: nil,
+		}
+		if err := bazfstestutil.CheckDir(mnt2.Dir, check); err != nil {
+			t.Error(err)
+		}
+	}
+
+	// cause a conflict
+	if err := ioutil.WriteFile(path.Join(mnt1.Dir, filename1), []byte(input2), 0644); err != nil {
+		t.Fatalf("cannot create file: %v", err)
+	}
+	if err := ioutil.WriteFile(path.Join(mnt2.Dir, filename1), []byte(input3), 0644); err != nil {
+		t.Fatalf("cannot create file: %v", err)
+	}
+
+	// sync backward
+	ctrl1 := controltest.ListenAndServe(t, &wg, app1)
+	defer ctrl1.Close()
+	rpcConn1, err := grpcunix.Dial(filepath.Join(app1.DataDir, "control"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rpcConn1.Close()
+	rpcClient1 := wire.NewControlClient(rpcConn1)
+	{
+		ctx := context.Background()
+		req := &wire.VolumeSyncRequest{
+			VolumeName: volumeName1,
+			Pub:        pub2[:],
+		}
+		if _, err := rpcClient1.VolumeSync(ctx, req); err != nil {
+			t.Fatalf("error while syncing: %v", err)
+		}
+	}
+
+	{
+		buf, err := ioutil.ReadFile(path.Join(mnt1.Dir, filename1))
+		if err != nil {
+			t.Fatalf("cannot read file: %v", err)
+		}
+		if g, e := string(buf), input2; g != e {
+			t.Fatalf("wrong content: %q != %q", g, e)
+		}
+	}
+
+	var seen os.FileInfo
+	entryCheckers := map[string]bazfstestutil.FileInfoCheck{
+		"": func(fi os.FileInfo) error {
+			if seen != nil {
+				return fmt.Errorf("expected only one file, already saw %q", seen.Name())
+			}
+			seen = fi
+			return nil
+		},
+	}
+	if err := bazfstestutil.CheckDir(path.Join(mnt1.Dir, ".bazil", "pending", filename1), entryCheckers); err != nil {
+		t.Error(err)
+	}
+	if seen == nil {
+		t.Fatal("expected to see a pending clock")
+	}
+
+	// declare the conflict handled
+	if err := os.Remove(path.Join(mnt1.Dir, ".bazil", "pending", filename1, seen.Name())); err != nil {
+		t.Fatalf("error removing pending entry: %v", err)
+	}
+
+	// rename the original
+	if err := os.Rename(path.Join(mnt1.Dir, filename1), path.Join(mnt1.Dir, filename2)); err != nil {
+		t.Fatal(err)
+	}
+
+	// sync again
+	{
+		ctx := context.Background()
+		req := &wire.VolumeSyncRequest{
+			VolumeName: volumeName2,
+			Pub:        pub1[:],
+		}
+		if _, err := rpcClient2.VolumeSync(ctx, req); err != nil {
+			t.Fatalf("error while syncing: %v", err)
+		}
+	}
+
+	{
+		check := map[string]bazfstestutil.FileInfoCheck{
+			filename2: nil,
+		}
+		if err := bazfstestutil.CheckDir(mnt2.Dir, check); err != nil {
+			t.Error(err)
+		}
+	}
+
+	{
+		buf, err := ioutil.ReadFile(path.Join(mnt2.Dir, filename2))
+		if err != nil {
+			t.Fatalf("cannot read file: %v", err)
+		}
+		if g, e := string(buf), input2; g != e {
 			t.Fatalf("wrong content: %q != %q", g, e)
 		}
 	}
