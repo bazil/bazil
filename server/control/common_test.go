db41fa9146605843111976e3b62046cd254a2479
Tommi Virtanen <tv@eagain.net>
2015-10-19 18:09:19 -0700

    server/control test: Cleaner test for error returned
    Made possible by https://github.com/grpc/grpc-go/issues/110 getting
    fixed.

--- server/control/common_test.go
+++ server/control/common_test.go
@@ -11,8 +11,7 @@ func checkRPCError(err error, code codes.Code, message string) error {
 	if g, e := grpc.Code(err), code; g != e {
 		return fmt.Errorf("wrong grpc error code: %v != %v", g, e)
 	}
-	// TODO https://github.com/grpc/grpc-go/issues/110
-	if g, e := err.Error(), fmt.Sprintf("rpc error: code = %d desc = %q", grpc.Code(err), message); g != e {
+	if g, e := grpc.ErrorDesc(err), message; g != e {
 		return fmt.Errorf("wrong error message: %v != %v", g, e)
 	}
 	return nil
