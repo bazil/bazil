7d170c1d94eef75d1af52332f738ed1f5081809c
Tommi Virtanen <tv@eagain.net>
2019-12-29 10:20:23 -0700

    Chase gRPC API changes: grpc.ErrorDesc

--- server/control/common_test.go
+++ server/control/common_test.go
@@ -3,7 +3,6 @@ package control_test
 import (
 	"fmt"
 
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -12,7 +11,7 @@ func checkRPCError(err error, code codes.Code, message string) error {
 	if g, e := status.Code(err), code; g != e {
 		return fmt.Errorf("wrong grpc error code: %v != %v", g, e)
 	}
-	if g, e := grpc.ErrorDesc(err), message; g != e {
+	if g, e := status.Convert(err).Message(), message; g != e {
 		return fmt.Errorf("wrong error message: %v != %v", g, e)
 	}
 	return nil
